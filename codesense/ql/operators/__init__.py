d6ec890c9868e79bea1b0909a368c2a0330a325d
old6ma <1220426338@qq.com>
2026-09-08 16:27:43 +0800

    feat: add evidence preserving graph projection

--- codesense/ql/operators/__init__.py
+++ codesense/ql/operators/__init__.py
@@ -2,6 +2,7 @@
 
 from codesense.ql.operators.hop import DEFAULT_MAX_DEGREE, DEFAULT_MAX_PATHS, hop, reach
 from codesense.ql.operators.intent import DEFAULT_MAX_ITEMS, intent
+from codesense.ql.operators.project import project
 from codesense.ql.operators.select import degree, only, score_of, top
 from codesense.ql.operators.unit import eval_unit
 
@@ -14,6 +15,7 @@ __all__ = [
     "hop",
     "intent",
     "only",
+    "project",
     "reach",
     "score_of",
     "top",
